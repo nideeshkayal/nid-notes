5d823a36a2b9734c2bc208a15666a8aaa524f642
nideeshkayal <nideeshkayal@gmail.com>
2026-03-12 18:28:35 +0530

    chore: add shiki rehype type declaration

--- src/rehype-shiki.d.ts
+++ src/rehype-shiki.d.ts
@@ -1,1 +1,9 @@
-declare module '@shikijs/rehype';
+declare module 'rehype-shiki' {
+  const rehypeShiki: any;
+  export default rehypeShiki;
+}
+
+declare module '@shikijs/rehype' {
+  const rehypeShiki: any;
+  export default rehypeShiki;
+}
